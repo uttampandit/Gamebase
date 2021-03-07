05eff3159b413fcf519d31a89217b15acedd59b6
uttampandit <amitapndt@gmail.com>
2021-03-07 23:14:02 +0530

    mid evaluation

--- main-pagejs.js
+++ main-pagejs.js
@@ -1,6 +1,9 @@
 firebase.auth.Auth.Persistence.LOCAL;
-
+const upload = document.querySelector('#upload');
+const search= document.getElementById("searchbar");
 const signout= document.getElementById("signoutbutton");
+const home= document.getElementById('home');
+const searchbutton= document.getElementById('search');
 
 signout.addEventListener("click",e=>{
     auth.signOut();
@@ -8,11 +11,33 @@ signout.addEventListener("click",e=>{
 
 auth.onAuthStateChanged(user=> {
     if(!user){
-     window.location.href='login-page.html';
+     window.location.href='home-page.html';
     
+    
+    }else{
+        search.addEventListener('click', ()=>{
+            search.style.width="800px";
+        })
+
+        database.collection('users').where('email','==', user.email).get().then((snapshot =>{
+            snapshot.docs.forEach((doc)=>{
+                if(doc.data().admin==true){
+                    upload.style.display='block';
+                    search.addEventListener('click', ()=>{
+                        search.style.width="700px";
+                    })
+                   
+                    search.style.left="320px";
+                  
+                    searchbutton.style.marginLeft="870px";
+                }
+            })
+        }))
     }
 }); 
-const user = auth.currentUser;
-const email = user.email;
+
+
+
+
  
-  console.log(email);
+  
---
+++ signupjs.js
@@ -0,0 +1,33 @@
+const button = document.getElementById('signUp');
+const form = document.querySelector('.loginform');
+const message= document.querySelector("#message");
+
+
+
+button.addEventListener('click', (e)=> {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+   
+    auth.createUserWithEmailAndPassword(email,password).then(cred => {
+       
+        document.getElementById('error').innerHTML='';
+    
+    }).catch(err => {
+        document.getElementById('error').innerHTML=err.message;
+    })
+    
+   
+})
+auth.onAuthStateChanged(user=> {
+    if(user){
+        database.collection('users').add({
+            username : form.username.value,
+            email : user.email,
+            admin : false
+        });
+         
+        message.innerHTML="Account created!<a href='main-page.html'>click here to redirect to main page</a>"
+     
+    }
+}); 
